60efc496e644baa5dcb34f658f47a261dfe2d956
k-orolevsk-y <i@korolevsky.me>
2023-09-03 18:54:49 +0300

    add agent metrics

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,65 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var (
+	poolInterval   = time.Duration(2)
+	reportInterval = time.Duration(10)
+)
+
+const APIUrl = "http://localhost:8080"
+
+func main() {
+	var metrics RuntimeMetrics
+	metrics.New()
+
+	for {
+		go func() {
+			err := metrics.Renew()
+			if err != nil {
+				panic(err)
+			}
+		}()
+
+		updateMetrics(&metrics)
+	}
+}
+
+func updateMetrics(m *RuntimeMetrics) {
+	time.Sleep(time.Second * reportInterval)
+
+	for k, v := range m.Runtime {
+		if err := updateMetric(k, v); err != nil {
+			panic(err)
+		}
+	}
+
+	if err := updateMetric("PollCount", m.PollCount); err != nil {
+		panic(err)
+	}
+
+	if err := updateMetric("RandomValue", m.RandomValue); err != nil {
+		panic(err)
+	}
+}
+
+func updateMetric(name string, metric Metric) error {
+	var value interface{}
+
+	if metric.Type == GaugeType {
+		valueFloat64 := metric.Value.(float64)
+		value = strconv.FormatFloat(valueFloat64, 'f', 1, 64)
+	} else {
+		value = metric.Value.(int64)
+	}
+
+	url := fmt.Sprintf("%s/update/%s/%s/%v", APIUrl, metric.Type, name, value)
+
+	_, err := http.Post(url, "text/plain", nil)
+	return err
+}
---
+++ cmd/agent/metrics.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"errors"
+	"math/rand"
+	"runtime"
+	"time"
+)
+
+type (
+	MetricType string
+	Metric     struct {
+		Type  MetricType
+		Value interface{}
+	}
+)
+
+var (
+	GaugeType   MetricType = "gauge"
+	CounterType MetricType = "counter"
+
+	ErrorInvalidPoolCount = errors.New("invalid poll count")
+)
+
+type RuntimeMetrics struct {
+	Runtime     map[string]Metric
+	PollCount   Metric
+	RandomValue Metric
+}
+
+func (m *RuntimeMetrics) New() {
+	m.Runtime = make(map[string]Metric)
+	m.PollCount = Metric{Type: CounterType, Value: int64(0)}
+	m.RandomValue = Metric{Type: GaugeType, Value: float64(0)}
+}
+
+func (m *RuntimeMetrics) Renew() error {
+	time.Sleep(time.Second * poolInterval)
+
+	var runtimeMetrics runtime.MemStats
+	runtime.ReadMemStats(&runtimeMetrics)
+
+	m.Runtime["Alloc"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.Alloc)}
+	m.Runtime["BuckHashSys"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.BuckHashSys)}
+	m.Runtime["GCCPUFraction"] = Metric{Type: GaugeType, Value: runtimeMetrics.GCCPUFraction}
+	m.Runtime["HeapAlloc"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.HeapAlloc)}
+	m.Runtime["HeapIdle"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.HeapIdle)}
+	m.Runtime["HeapInuse"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.HeapInuse)}
+	m.Runtime["HeapObjects"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.HeapObjects)}
+	m.Runtime["HeapReleased"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.HeapReleased)}
+	m.Runtime["HeapSys"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.HeapSys)}
+	m.Runtime["LastGC"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.LastGC)}
+	m.Runtime["Lookups"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.Lookups)}
+	m.Runtime["MCacheInuse"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.MCacheInuse)}
+	m.Runtime["MCacheSys"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.MCacheSys)}
+	m.Runtime["MSpanInuse"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.MSpanInuse)}
+	m.Runtime["Mallocs"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.Mallocs)}
+	m.Runtime["NumForcedGC"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.NumForcedGC)}
+	m.Runtime["NumGC"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.NumGC)}
+	m.Runtime["OtherSys"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.OtherSys)}
+	m.Runtime["PauseTotalNs"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.PauseTotalNs)}
+	m.Runtime["StackInuse"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.StackInuse)}
+	m.Runtime["StackSys"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.StackSys)}
+	m.Runtime["Sys"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.Sys)}
+	m.Runtime["TotalAlloc"] = Metric{Type: GaugeType, Value: float64(runtimeMetrics.TotalAlloc)}
+
+	pollCount, ok := m.PollCount.Value.(int64)
+	if !ok {
+		return ErrorInvalidPoolCount
+	}
+	m.PollCount.Value = pollCount + 1
+
+	m.RandomValue.Value = rand.Float64()
+	return nil
+}
