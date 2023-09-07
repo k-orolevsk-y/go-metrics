f9341c0e81164b77f83ac3663873c6ff3c1b789f
k-orolevsk-y <i@korolevsky.me>
2023-09-07 23:03:44 +0300

    create metrics updater and spread the function of sending the metric

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"github.com/go-resty/resty/v2"
 	"github.com/k-orolevsk-y/go-metricts-tpl/internal/agent/config"
 	"github.com/k-orolevsk-y/go-metricts-tpl/internal/agent/metrics"
-	"log"
-	"strconv"
-	"time"
-)
-
-var (
-	ErrorInvalidMetricValueType = errors.New("invalid metric value type")
+	metricsupdater "github.com/k-orolevsk-y/go-metricts-tpl/internal/agent/metrics_updater"
 )
 
 func main() {
@@ -20,69 +12,20 @@ func main() {
 		panic(err)
 	}
 
-	var metricsStore metrics.RuntimeMetrics
-	metricsStore.Init()
+	var store metrics.RuntimeMetrics
+	store.Init()
 
-	restyClient := resty.New()
+	client := resty.New()
+	updater := metricsupdater.New(client, &store)
+
 	for {
 		go func() {
-			err := metricsStore.Update()
+			err := store.Update()
 			if err != nil {
 				panic(err)
 			}
 		}()
 
-		updateMetrics(&metricsStore, restyClient)
+		updater.UpdateMetrics()
 	}
-}
-
-func updateMetrics(metricsStore *metrics.RuntimeMetrics, restyClient *resty.Client) {
-	time.Sleep(time.Second * time.Duration(config.Data.ReportInterval))
-
-	for k, v := range metricsStore.Runtime {
-		if err := updateMetric(k, v, restyClient); err != nil {
-			log.Printf("[Warning] %s - %v", k, err)
-		}
-	}
-
-	if err := updateMetric("PollCount", metricsStore.PollCount, restyClient); err != nil {
-		log.Printf("[Warning] PollCount - %v", err)
-	}
-
-	if err := updateMetric("RandomValue", metricsStore.RandomValue, restyClient); err != nil {
-		log.Printf("[Warning] RandomValue - %v", err)
-	}
-}
-
-func updateMetric(name string, metric metrics.Metric, restyClient *resty.Client) error {
-	var value interface{}
-
-	switch metric.Value.(type) {
-	case float64:
-		if metric.Type != metrics.GaugeType {
-			return ErrorInvalidMetricValueType
-		}
-
-		valueFloat64 := metric.Value.(float64)
-		value = strconv.FormatFloat(valueFloat64, 'f', 1, 64)
-	case int64:
-		if metric.Type != metrics.CounterType {
-			return ErrorInvalidMetricValueType
-		}
-
-		value = metric.Value.(int64)
-	default:
-		return ErrorInvalidMetricValueType
-	}
-
-	url := fmt.Sprintf("http://%s/update/%s/%s/%v", config.Data.Address, metric.Type, name, value)
-
-	_, err := restyClient.R().
-		SetHeader("Content-Type", "text/plain").
-		Post(url)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
